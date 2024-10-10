7925373cc5f544c4229111570cce75be9f4e41f7
Jacky Lee <jackywhlee@gmail.com>
2024-10-10 13:15:55 +0800

    setup frontend folder structure, enable cors, add apollo cient gql on frontend and test loading mongodb atlas data on main page

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log', 'debug', 'verbose'],
   });
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:3001', // Allow requests from your Next.js frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
